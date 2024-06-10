ec290f725572570473d25e36b18bd2d73f4a45a2
eneriopaolo <paoloenerio@gmail.com>
2024-06-10 21:55:21 +0800

    Repurposed Auth Fetch Req

---
+++ FRONTEND/src/lib/userauth.fetch.js
@@ -0,0 +1,49 @@
+async function loginUser(email, password){
+    const myHeaders = {
+        "Content-Type": "application/json"
+    };
+
+    let reqHeaders = new Headers(myHeaders);
+    let reqBody = JSON.stringify({
+        "email": email,
+        "password": password
+    });
+
+    let reqOptions = {
+        method: 'POST',
+        headers: reqHeaders,
+        body: reqBody
+    };
+
+    const URI = 'http://localhost:4000/api/auth/login';
+    const response = await fetch(URI, reqOptions);
+    return response
+};
+
+async function registerUser(email, name, password){
+    const myHeaders = {
+        "Content-Type": "application/json"
+    };
+
+    let reqHeaders = new Headers(myHeaders);
+    let reqBody = JSON.stringify({
+        "email": email,
+        "name": name,
+        "password": password
+    });
+
+    let reqOptions = {
+        method: 'POST',
+        headers: reqHeaders,
+        body: reqBody
+    };
+
+    const URI = 'http://localhost:4000/api/auth/register';
+    const response = await fetch(URI, reqOptions);
+    return response
+};
+
+export {
+    loginUser,
+    registerUser
+};
